6ea2babf3bbbb2c8f16c78fbf0a2f08658c113c0
Mikhail Brinchuk <thecentury@gmail.com>
2023-06-25 15:42:32 +0200

    Removed trailing spaces

--- src/Avalonia.FuncUI.Elmish/ElmishHook.fs
+++ src/Avalonia.FuncUI.Elmish/ElmishHook.fs
@@ -7,7 +7,7 @@ open Elmish
 type ElmishState<'model, 'msg, 'arg>(mkProgram : unit -> Program<'arg, 'model, 'msg, unit>, arg: 'arg, setModel) =
     let program = mkProgram()
 
-    let mutable _model = Program.init program arg |> fst
+    let mutable _model : 'model = Program.init program arg |> fst
     let mutable _dispatch = fun (_: 'msg) -> ()
 
     let setState model dispatch =
@@ -30,46 +30,46 @@ let ignoreView = (fun _ _ -> ())
 type IComponentContext with
 
     /// Starts an Elmish loop with an existing IWritable.
-    member this.useElmish<'model, 'msg> 
+    member this.useElmish<'model, 'msg>
         (
-            writableModel: IWritable<'model>, 
-            update: 'msg -> 'model -> 'model * Cmd<'msg>, 
+            writableModel: IWritable<'model>,
+            update: 'msg -> 'model -> 'model * Cmd<'msg>,
             ?mapProgram: Program<unit, 'model, 'msg, unit> -> Program<unit, 'model, 'msg, unit>
         ) =
-        
+
         let mapProgram = defaultArg mapProgram id
         let elmishState = this.useState(None, false)
 
         // Start Elmish loop
         this.useEffect(
-            fun () -> 
-                let mkProgram() = 
+            fun () ->
+                let mkProgram() =
                     let init() = writableModel.Current, Cmd.none
-                    Program.mkProgram init update ignoreView 
+                    Program.mkProgram init update ignoreView
                     |> mapProgram
 
-                ElmishState(mkProgram, (), writableModel.Set) 
-                |> Some 
+                ElmishState(mkProgram, (), writableModel.Set)
+                |> Some
                 |> elmishState.Set
 
             , [ EffectTrigger.AfterInit ]
         )
-        
-        let dispatch map = 
+
+        let dispatch map =
             elmishState.Current
             |> Option.iter (fun es -> es.Dispatch map)
 
         writableModel.Current, dispatch
-    
+
     /// Starts an Elmish loop with an init arg.
-    member this.useElmish<'arg, 'model, 'msg> 
+    member this.useElmish<'arg, 'model, 'msg>
         (
-            init : 'arg -> 'model * Cmd<'msg>, 
-            update: 'msg -> 'model -> 'model * Cmd<'msg>, 
-            initArg: 'arg, 
+            init : 'arg -> 'model * Cmd<'msg>,
+            update: 'msg -> 'model -> 'model * Cmd<'msg>,
+            initArg: 'arg,
             ?mapProgram: Program<'arg, 'model, 'msg, unit> -> Program<'arg, 'model, 'msg, unit>
         ) =
-        
+
         let mapProgram = mapProgram |> Option.defaultValue id
 
         let elmishState = this.useState(None, false)
@@ -77,29 +77,29 @@ type IComponentContext with
 
         // Start Elmish loop
         this.useEffect(
-            fun () -> 
-                let mkProgram() = 
+            fun () ->
+                let mkProgram() =
                     Program.mkProgram init update ignoreView
                     |> mapProgram
 
-                ElmishState(mkProgram, initArg, writableModel.Set) 
-                |> Some 
+                ElmishState(mkProgram, initArg, writableModel.Set)
+                |> Some
                 |> elmishState.Set
 
             , [ EffectTrigger.AfterInit ]
         )
-        
-        let dispatch map = 
+
+        let dispatch map =
             elmishState.Current
             |> Option.iter (fun es -> es.Dispatch map)
 
         writableModel.Current, dispatch
 
     /// Starts an Elmish loop.
-    member this.useElmish<'model, 'msg> 
+    member this.useElmish<'model, 'msg>
         (
-            init : unit -> 'model * Cmd<'msg>, 
-            update: 'msg -> 'model -> 'model * Cmd<'msg>, 
+            init : unit -> 'model * Cmd<'msg>,
+            update: 'msg -> 'model -> 'model * Cmd<'msg>,
             ?mapProgram: Program<unit, 'model, 'msg, unit> -> Program<unit, 'model, 'msg, unit>
         ) =
 
